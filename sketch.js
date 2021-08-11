58035c30c70e11847691bc6fa5a23ad62c1811be
SP1411 <85595663+SP1411@users.noreply.github.com>
2021-08-11 14:59:12 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,97 @@
+var astronaut;
+
+
+
+function preload() {
+
+  bg = loadImage("images/iss.png");
+  sleep = loadAnimation("images/sleep.png");
+  brush = loadAnimation("images/brush.png");
+  gym = loadAnimation("images/gym1.png", "images/gym2.png", "images/gym11.png", "images/gym12.png");
+  eat = loadAnimation("images/eat1.png", "images/eat2.png");
+  drink = loadAnimation("images/drink1.png", "images/drink2.png");
+  move = loadAnimation("images/move.png", "images/move1.png");
+  bath = loadAnimation("images/bath1.png", "images/bath2.png");
+
+}
+
+
+function setup() {
+  createCanvas(800,400);
+  
+  
+
+  astronaut = createSprite(300,230);
+  astronaut.addAnimation("sleeping", sleep);
+  astronaut.scale = 0.1;
+}
+
+function draw() {
+  background(bg);
+  
+  fill("white");
+  textSize(20);
+  text("Instructions:", 55, 27);
+
+  textSize(15);
+  text("Up Arrow = Brushing", 55, 45);
+  text("Down Arrow = Gymming", 55, 58);
+  text("Left Arrow = Eating", 55, 71);
+  text("Right Arrow = Bathing", 55, 84);
+  text("m key = Moving", 55, 97)
+
+  edges = createEdgeSprites();
+  astronaut.bounceOff(edges);
+
+  if(keyCode === UP_ARROW) {
+
+    astronaut.addAnimation("brushing", brush);
+    astronaut.changeAnimation("brushing");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+
+  }
+
+  if(keyCode === DOWN_ARROW) {
+
+    astronaut.addAnimation("gymming", gym);
+    astronaut.changeAnimation("gymming");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+    
+  }
+
+  if(keyCode === RIGHT_ARROW) {
+
+    astronaut.addAnimation("bathing", bath);
+    astronaut.changeAnimation("bathing");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+    
+  }
+
+  if(keyCode === LEFT_ARROW) {
+
+    astronaut.addAnimation("eating", eat);
+    astronaut.changeAnimation("eating");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+    
+  }
+
+  if(keyCode === "m") {
+
+    astronaut.addAnimation("moving", move);
+    astronaut.changeAnimation("moving");
+    astronaut.velocityX = 1;
+    astronaut.velocityY = 1;
+    
+  }
+
+  drawSprites();
+
+}
